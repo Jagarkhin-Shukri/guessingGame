4d08a0be7dee03b76c20b3e8c985d5d21964761a
Jagarkhin Shukri <Jagarkhin.shukri1@outlook.com>
2025-10-21 16:25:35 +0200

    Implement start() method and call it in main

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,6 +2,8 @@ public class GuessingGame {
   public static void main(String[] args) {
 
     Guesser guesser = new Guesser(0, 1000);
-    System.out.println("Guesser-objektet");
+    System.out.println("Guesser-objekt");
+
+    guesser.start();
   }
 }
